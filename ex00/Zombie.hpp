2a778c088b0dd1e4c861cdaa6c9a35cc70c104bb
atamas <tamas.andor@yahoo.com>
2024-10-22 17:40:31 +0200

    Correct Makefiles and some norminette even though its not needed

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,9 +6,12 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 12:43:13 by atamas            #+#    #+#             */
-/*   Updated: 2024/10/21 13:34:14 by atamas           ###   ########.fr       */
+/*   Updated: 2024/10/21 14:21:07 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
 
 #include <iostream>
 
@@ -25,3 +28,5 @@ public:
 
 Zombie*	newZombie( std::string name );
 void	randomChump(std::string name);
+
+#endif
